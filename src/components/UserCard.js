e293f4870cf4bd0ad1b9817fcbdfc7cdfe9dbaea
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-09-03 16:42:20 -0500

    completed MVP, moving on to input stretch

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import axios from 'axios';
-import { Card, Grid, Icon } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const MainCard = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 5%;
+
+`;
+
+const FollowerCard = styled.div`
+margin: 2%;
+justify-content: center;
+`;
 
 
 
@@ -24,28 +38,39 @@ class UserCard extends React.Component {
     return (
         <>
         {this.props.user.map(newUser => (
+            <div key = {newUser.id}>
+            <MainCard>
+                <h1>Github User:</h1>
             <Card
+            href={`https://github.com/${newUser.login}`}
             image={newUser.avatar_url}
             header={newUser.name}
             meta={`Following: ${newUser.following}`}
             description={`Bio: ${newUser.bio}`}
             extra={`${newUser.followers} Followers`}
             />
+            </MainCard>
+            </div>
 
             
         ))}
        
        <h1>Andrew's Followers</h1>
-       <Grid relaxed columns={3}>
-           <Grid.Row padded='vertically' columns={3}>
+       <Grid centered columns={3}>
+            <Grid.Row padded='vertically' columns={3}>
         {this.state.followers.map(newFollower => (
+            <div key={newFollower.id}>
+                <FollowerCard>
              <Card
+             href={`https://github.com/${newFollower.login}`}
              image={newFollower.avatar_url}
              header={newFollower.login}
              />
+                </FollowerCard>
+            </div>
 
         ))}
-        </Grid.Row>
+            </Grid.Row>
         </Grid>
         </>
     );
